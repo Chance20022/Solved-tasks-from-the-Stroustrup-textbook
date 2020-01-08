3af5d561e19061dc9f80208fec28cd491b5f2d1c
Chance20022 <chance20022@gmail.com>
2020-01-08 18:56:37 +0300

    Exercise on page 169

---
+++ Page 169/Exercise 3 on page 169/Exercise on page 169/Exercise on page 169.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    //exercise 2 on page 169
+
+    vector<float> temps; // Вектор, который будет держать в себе значения темпиратуры
+    float result = 0; //Чтобы избежать не целочисленных чисел при вычислении медианы
+    for (float temp; cin >> temp;)
+        temps.push_back(temp);
+
+    cout << endl;
+
+    if (temps.size() / 2 % 2)// Чтобы избавиться от остатка при делении на нечётные числа
+        result = temps[temps.size() / 2] + 1; // все чётные числа, делящиеся на 2, не имеют остатка. И если при делении на 2 есть остаток
+                                              //То прибавляем 1, чтобы избежать остаток (по заданию допускается)
+    else result = temps[temps.size() / 2];
+
+    cout << "Temperature median - " << result << endl;
+
+    //exercise 3 on page 169
+    
+    
+    float sum = 0;
+    for (float s : temps) sum += s;
+    cout << "Sum of all distances - " << sum << endl;
+
+    float min = 0;
+    float max = 0;
+
+    sort(temps.begin(), temps.end());
+
+    cout << "Minimal distans of cities - " << temps[0] << endl;
+
+    int counter = 0;
+
+    for (int i = 1; i < temps.size(); i++)
+        counter++;
+
+    cout << "Maximum distans of cities - " << temps[counter] << endl;
+    cout << endl;
+
+    // Я ебал эту сортировку пузырьком, она тупо не работает для векторов. Я потратил целый день зря
+
+    // exercise 4 on page 169
+
+
+}
---
+++ Page 169/Exercise 5 on page 169/Exercise 5 on page 169/Exercise 5 on page 169.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a = 0, b = 0, sum = 0;
+    char operation = ' ';
+
+    cout << "Enter the expression to be counted(Separated by spaces) - " << endl;
+    cout << "Your the first number - ", cin >> a;
+    cout << endl;
+    cout << "Your the second number - ", cin >> b;
+    cout << endl;
+    cout << "Your operation - ", cin >> operation;
+
+    if (operation == '+') {
+        sum = a + b;
+        cout << "Sum " << a << " and " << b << " equals " << sum << endl;
+    }
+    else if (operation == '-') {
+        sum = a - b;
+        cout << "Difference " << a << " and " << b << " equals " << sum << endl;
+    }
+    else if (operation == '*') {
+        sum = a * b;
+        cout << "Product " << a << " and " << b << " equals " << sum << endl;
+    }
+    else if (operation == '/') {
+        sum = a / b;
+        cout << "Dvision " << a << " and " << b << " equals " << sum << endl;
+    }
+
+    return 0;
+}
