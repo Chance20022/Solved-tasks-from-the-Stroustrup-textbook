5bdd007c2b8b2ac591eee9f9488fac6d6f267697
Chance20022 <chance20022@gmail.com>
2020-01-20 01:54:58 +0300

    I want to eat

--- Chapter 4/Assignments after Chapter 4/Task 7/Task 7/Task 7.cpp
+++ Chapter 4/Assignments after Chapter 4/Task 7/Task 7/Task 7.cpp
@@ -5,43 +5,39 @@ using namespace std;
 
 int main()
 {
-    vector<string> numbWords = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-    string numforoper1;
-    string numforoper2;
-    char operation = ' ';
+    vector<string> numbWords = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
+    vector<string> numbers = { "0","1","2","3","4","5","6","7","8","9","10" };
+    string numforoper1; // Для воода первого числа
+    string numforoper2; // для ввода второго числа
+    char operation = ' '; // Для воода оператора
+    int answerInWords1 = 0; // Для переводда из слов в число из переменной numforoper1
+    int answerInWords2 = 0; // Для переводда из слов в число из переменной numforoper2
+    int answer = 0;
 
     cout << "Input your the first number - ", cin >> numforoper1;
     cout << endl;
-
     cout << "Input your the second number - ", cin >> numforoper2;
     cout << endl;
     cout << "Input your operation - ", cin >> operation;
     cout << endl;
 
-
-
-
-    /*
-    vector<int> numbers = { 0,1,2,3,4,5,6,7,8,9 };
-    int numb = 0;
-    string numbW;
-    int choice = 0;
-    cout << "What you want? From uppercase to numeric (1) or from numeric to uppercase (0)? - ", cin >> choice;
-    cout << endl;
-
-    if (choice == 1) {
-        cout << "Input your number 0 to 9 - ", cin >> numb;
-        cout << endl;
-        cout << "Youre number in uppercase - " << numbWords[numb];
+    for (int i = 0; i < numbWords.size(); i++) { // Преобразуем слова в числа
+        if (numforoper1 == numbWords[i])
+            answerInWords1 = i;
+        if (numforoper2 == numbWords[i])
+            answerInWords2 = i;
+        if (numforoper1 == numbers[i])
+            answerInWords1 = i;
+        if (numforoper2 == numbers[i])
+            answerInWords2 = i;
     }
-    else if (choice == 0) {
-        cout << "Input your number zero to nine - ", cin >> numbW;
-        cout << endl;
-        for (int i = 0; i < numbWords.size(); i++) {
-            if (numbW == numbWords[i]) {
-                cout << "Youre number in numeric - " << i;
-                break;
-            }
-        }
-    }*/
+
+    if (operation == '+')
+        cout << numforoper1 << operation << numforoper2 << "=" << answerInWords1 + answerInWords2;
+    if (operation == '-')
+        cout << numforoper1 << operation << numforoper2 << "=" << answerInWords1 - answerInWords2;
+    if (operation == '/')
+        cout << numforoper1 << operation << numforoper2 << "=" << answerInWords1 / answerInWords2;
+    if (operation == '*')
+        cout << numforoper1 << operation << numforoper2 << "=" << answerInWords1 * answerInWords2;
 }
