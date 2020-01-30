b7cd0c3439716dee1c6bf5a0d473bffa0284683c
Chance20022 <chance20022@gmail.com>
2020-01-30 12:51:28 +0300

    .

---
+++ Chapter 4/Assignments after Chapter 4/Task 11-12/Task 11-12/Task 11-12.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int check(int x) {
+    if (x == 0 || x == 1)
+        return false;
+    
+    for (int i = 2; i < x; i++) {
+        if (!(x % i))
+            return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    vector<int> basicNum;
+    int choice = 0;
+
+    cout << "Input your range - ", cin >> choice;
+    cout << endl;
+
+    for (int i = 0; i < choice; i++) {
+        if (check(i)) {
+            basicNum.push_back(i);
+        }
+    }
+
+    for (int x : basicNum) {
+        cout << x << ", ";
+    }
+}
---
+++ Chapter 4/Assignments after Chapter 4/Task 16/Task 16/Task 16.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> numbRandom = { 2,51,51,26,367,262,74,57,24,24,51,24,52,51,67,24,24,24,24,24,24,24,24,24,242,49,45,51 }; 
+	vector<int> repetNum;
+	int counter = 0;
+	int counterOFIndex = 0;
+	int max = 0;
+
+	for (int i = 0; i < numbRandom.size(); i++) {
+		for (int z = 0; z < numbRandom.size(); z++) {
+			if (numbRandom[i] == numbRandom[z]) {
+				counter++; // Счётчик повторяющегося элемента
+				numbRandom.erase(numbRandom.begin() + z); //удаление повторяющегося элемента
+			}
+			if (max < counter) {
+				max = counter;
+				counterOFIndex = numbRandom[i];
+			}
+
+		}
+		counter = counter - counter;
+
+	}
+
+	cout << "Largest repeating number is - " << counterOFIndex << ", repeated " << max << " times" << endl;
+}
