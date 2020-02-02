ab8b13e0e68c89bbc59d0a502c30985da06308a1
Chance20022 <chance20022@gmail.com>
2020-02-02 19:10:47 +0300

    Aga da

---
+++ Chapter 4/Assignments after Chapter 4/Task 17/Task 17/Task 17.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <random>
+
+using namespace std;
+
+int random() {
+    int randd = 0;
+
+    for (int i = 0; i < 20; i++) {
+        randd = rand();
+    }
+}
+
+int main()
+{
+    vector<int> numbers;
+    numbers.push_back(random());
+
+    for (int x : numbers)
+        cout << x << " ";
+}
---
+++ Chapter 4/Assignments after Chapter 4/Task 18/Task 18/Task 18.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a = 0, b = 0, c = 0, answer = 0, x1 = 0, x2 = 0;
+
+    cout << "Input number of a - ", cin >> a;
+    cout << endl;
+    cout << "Input number of b - ", cin >> b;
+    cout << endl;
+    cout << "Input number of c - ", cin >> c;
+    cout << endl;
+    cout << "You have - " << a << "x^2 + " << b << "x + " << c << endl;
+
+    answer = (b * b) - (4 * a * c);
+    x1 = (-b + sqrt(answer)) / (2 * a);
+    x2 = (-b - sqrt(answer)) / (2 * a);
+
+    cout << "X1 = " << x1 << ", X2 = " << x2 << endl;
+}
